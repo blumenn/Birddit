c9a0a53a8967a8b0128aaaffad384c321ffec9c1
benjjjjjj <302301@viauc.dk>
2022-10-05 12:22:19 +0200

    til og med slide 12 i guide 1.

---
+++ Application/DaoInterfaces/IUserDAO.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Models;
+
+namespace Application.DaoInterfaces;
+
+public interface IUserDAO
+{
+    Task<User> CreateAsync(User user);
+    Task<User?> GetByUsernameAsync(string userName);
+}
---
+++ Application/Logic/UserLogic.cs
@@ -0,0 +1,44 @@
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace Application.Logic;
+
+public class UserLogic : IUserLogic
+{
+    private readonly IUserDAO userDao;
+
+    public UserLogic(IUserDAO userDao)
+    {
+        this.userDao = userDao;
+    }
+
+    public async Task<User> CreateAsync(UserCreationDto dto)
+    {
+        User? existing = await userDao.GetByUsernameAsync(dto.UserName);
+        if (existing != null)
+            throw new Exception("Username already taken!");
+
+        ValidateData(dto);
+        User toCreate = new User
+        {
+            userName = dto.UserName
+        };
+        
+        User created = await userDao.CreateAsync(toCreate);
+        
+        return created;
+    }
+
+    private static void ValidateData(UserCreationDto userToCreate)
+    {
+        string userName = userToCreate.UserName;
+
+        if (userName.Length < 3)
+            throw new Exception("Username must be at least 3 characters!");
+
+        if (userName.Length > 15)
+            throw new Exception("Username must be less than 16 characters!");
+    }
+}
